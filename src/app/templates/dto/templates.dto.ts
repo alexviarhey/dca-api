ea429aa658a1e1b30d5933ef44aca03143245df9
alexviarhey <lexa.vergey@gmail.com>
2022-12-28 23:37:20 +0300

    added swagger to template dtos

--- src/app/templates/dto/templates.dto.ts
+++ src/app/templates/dto/templates.dto.ts
@@ -1,21 +1,42 @@
 import { TemplateType } from "../schemas/template.schema";
 import { ICDDto } from "../../icd/icd.dto";
-import { ServiceSubgroupDto, } from "../../price-list/dto/price-list.dtos";
+import { ServiceSubgroupDto } from "../../price-list/dto/price-list.dtos";
+import { ApiProperty } from "@nestjs/swagger";
 
 
 export class CreateTemplateDto {
-    icdId?: string
-    type: TemplateType
-    name: string
-    description: string
-    subgroupsIds?: string[]
+    @ApiProperty({ required: false })
+    icdId?: string;
+
+    @ApiProperty({ enum: TemplateType })
+    type: TemplateType;
+
+    @ApiProperty()
+    name: string;
+
+    @ApiProperty()
+    description: string;
+
+    @ApiProperty({ required: false, isArray: true })
+    subgroupsIds?: string[];
 }
 
 export class TemplateDto {
-    _id: string
-    icd: ICDDto
-    type: TemplateType
-    name: string
-    description: string
-    subgroups: ServiceSubgroupDto[]
+    @ApiProperty()
+    _id: string;
+
+    @ApiProperty({ type: () => ICDDto })
+    icd: ICDDto;
+
+    @ApiProperty({ enum: TemplateType })
+    type: TemplateType;
+
+    @ApiProperty()
+    name: string;
+
+    @ApiProperty()
+    description: string;
+
+    @ApiProperty({ type: () => ServiceSubgroupDto, isArray: true })
+    subgroups: ServiceSubgroupDto[];
 }
