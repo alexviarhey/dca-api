80fc9b2247cb009a8586214f807ac8a6819a4355
alexviarhey <lexa.vergey@gmail.com>
2023-04-07 20:10:12 +0300

    remove unused imports

--- src/app/practitioners/use-cases/practitioners.crud.use-cases.ts
+++ src/app/practitioners/use-cases/practitioners.crud.use-cases.ts
@@ -1,11 +1,10 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { CrudUseCases } from "../../../core/crud.use-cases";
-import { FilterQuery, Model, SaveOptions } from "mongoose";
+import { Model} from "mongoose";
 import { CreatePractitionerDto, PractitionerDto } from "../dto/practitioner.dto";
 import { PRACTITIONERS_COLLECTION, PractitionerSchema } from "../schemas/practitioner.schema";
 import { practitionersMapper } from "../mappers/practitioner.mapper";
-import { Result } from "../../../core/result";
 
 @Injectable()
 export class PractitionersCrudUseCases extends CrudUseCases<PractitionerSchema,
